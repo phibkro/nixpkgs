255b1102dde50cf8aba8db9e198f7560de1db07c
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-06-03 13:35:44 +0200

    python3Packages.matter-ble-proxy: init at 0.8.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9727,6 +9727,8 @@ self: super: with self; {
 
   matrix-nio = callPackage ../development/python-modules/matrix-nio { };
 
+  matter-ble-proxy = callPackage ../development/python-modules/matter-ble-proxy { };
+
   matter-python-client = callPackage ../development/python-modules/matter-python-client { };
 
   mattermostdriver = callPackage ../development/python-modules/mattermostdriver { };
